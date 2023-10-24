7495fbd86107fb91e3517971d93d05fa13f20211
wun-yu-lin <76545619+wun-yu-lin@users.noreply.github.com>
2023-10-24 13:40:03 +0800

    first commit

---
+++ src/main/java/wehelp/wunyu/wehelp_stage3_practice/WeHelpStage3PracticeApplication.java
@@ -0,0 +1,13 @@
+package wehelp.wunyu.wehelp_stage3_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeHelpStage3PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeHelpStage3PracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wehelp/wunyu/wehelp_stage3_practice/WeHelpStage3PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package wehelp.wunyu.wehelp_stage3_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeHelpStage3PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
